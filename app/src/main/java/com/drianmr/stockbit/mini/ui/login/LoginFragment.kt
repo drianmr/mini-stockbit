2cd618a32f7c2f56bfc45b3278cbd6fde9864dee
Adrian M. R <adrianmridwan@gmail.com>
2021-06-05 16:37:12 +0700

    Add register button in login screen

--- app/src/main/java/com/drianmr/stockbit/mini/ui/login/LoginFragment.kt
+++ app/src/main/java/com/drianmr/stockbit/mini/ui/login/LoginFragment.kt
@@ -1,9 +1,14 @@
 package com.drianmr.stockbit.mini.ui.login
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.buildSpannedString
+import androidx.core.text.color
+import com.drianmr.stockbit.mini.R
 import com.drianmr.stockbit.mini.databinding.FragmentLoginBinding
+import com.drianmr.stockbit.mini.ext.getColorCompat
 import com.drianmr.stockbit.mini.ui.base.BaseUiFragment
 
 class LoginFragment : BaseUiFragment() {
@@ -14,5 +19,29 @@ class LoginFragment : BaseUiFragment() {
     override fun onCreateViewBinding(inflater: LayoutInflater, container: ViewGroup?): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setup()
+    }
+
+    private fun setup() {
+        setupRegisterButton()
+    }
+
+    private fun setupRegisterButton() {
+        val black = getColorCompat(R.color.black_text)
+        val brand = getColorCompat(R.color.brand)
+
+        val text = getString(R.string.action_dont_have_account)
+        val text1 = text.substring(0, text.indexOf('?') + 1)
+        val text2 = text.substring(text.indexOf('?') + 1, text.length)
+
+        binding.buttonRegister.text = buildSpannedString {
+            color(black) { append(text1) }
+            color(brand) { append(text2) }
+        }
     }
 }
