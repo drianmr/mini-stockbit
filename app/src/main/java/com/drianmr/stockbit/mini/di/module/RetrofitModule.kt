6437496ae7ba317224a6b4f62ab89546486709bc
Adrian M. R <adrianmridwan@gmail.com>
2021-06-05 23:09:31 +0700

    Add retrofit module

---
+++ app/src/main/java/com/drianmr/stockbit/mini/di/module/RetrofitModule.kt
@@ -0,0 +1,25 @@
+package com.drianmr.stockbit.mini.di.module
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://min-api.cryptocompare.com/data/")
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+}
