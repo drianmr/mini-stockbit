824b841995a1b023c2bef8cce5d5e4c25abd6d25
Adrian M. R <adrianmridwan@gmail.com>
2021-06-06 03:16:01 +0700

    Disable night theme

--- app/src/main/java/com/drianmr/stockbit/mini/MiniStockbitApplication.kt
+++ app/src/main/java/com/drianmr/stockbit/mini/MiniStockbitApplication.kt
@@ -1,7 +1,16 @@
 package com.drianmr.stockbit.mini
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MiniStockbitApplication : Application()
+class MiniStockbitApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Disable night theme
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
