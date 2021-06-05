7dc694e8f7e3e292cdbe642726b9c529dd511530
Adrian M. R <adrianmridwan@gmail.com>
2021-06-06 01:51:01 +0700

    Implemented format price

--- app/src/main/java/com/drianmr/stockbit/mini/ui/watchlist/WatchlistViewHolder.kt
+++ app/src/main/java/com/drianmr/stockbit/mini/ui/watchlist/WatchlistViewHolder.kt
@@ -1,18 +1,44 @@
 package com.drianmr.stockbit.mini.ui.watchlist
 
 import androidx.recyclerview.widget.RecyclerView
+import com.drianmr.stockbit.mini.R
 import com.drianmr.stockbit.mini.databinding.ItemWatchlistBinding
+import com.drianmr.stockbit.mini.ext.resources
 import com.drianmr.stockbit.mini.model.Coin
+import java.text.DecimalFormat
+import kotlin.math.abs
 
 class WatchlistViewHolder(
     private val binding: ItemWatchlistBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(data: Coin) {
+        val rawPrice = data.raw?.idr?.price ?: 0f
+        val rawChangeDay = data.raw?.idr?.changeDay ?: 0f
+
+        val rawChangeDayPercentage =
+            if (abs(rawPrice) > 0f && abs(rawChangeDay) > 0) {
+                (rawChangeDay / rawPrice) * 100
+            } else {
+                0f
+            }
+
+        val changeDayPercentage =
+            if (rawChangeDayPercentage > 0f) "+$rawChangeDayPercentage" else "$rawChangeDayPercentage"
+
         binding.textViewName.text = data.info.name
         binding.textViewFullName.text = data.info.fullName
-        // TODO: Format
-        binding.textViewPrice.text = data.raw?.idr?.price?.toString() ?: "0.0"
-        binding.textViewChangeDay.text = data.raw?.idr?.changeDay?.toString() ?: "0.0 (0%)"
+        binding.textViewPrice.text = formatPrice(rawPrice)
+        binding.textViewChangeDay.text =
+            resources.getString(
+                R.string.template_change_day,
+                formatPrice(rawChangeDay),
+                changeDayPercentage,
+            )
+    }
+
+    private fun formatPrice(price: Float): String {
+        val value = DecimalFormat("#,###.#").format(price)
+        return if (price > 0f) "+$value" else value
     }
 }
