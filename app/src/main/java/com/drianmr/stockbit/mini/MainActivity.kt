5ae3758ee73c16f44fea22853bc335d3cb775d76
Adrian M. R <adrianmridwan@gmail.com>
2021-06-05 16:54:06 +0700

    Fix toolbar title not sync with navigation fragment label

--- app/src/main/java/com/drianmr/stockbit/mini/MainActivity.kt
+++ app/src/main/java/com/drianmr/stockbit/mini/MainActivity.kt
@@ -2,11 +2,15 @@ package com.drianmr.stockbit.mini
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupWithNavController
 import com.drianmr.stockbit.mini.databinding.ActivityMainBinding
+import com.drianmr.stockbit.mini.ext.findNavController
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +18,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        init()
         setup()
+    }
+
+    private fun init() {
+        navController = findNavController(R.id.nav_host_fragment)
     }
 
     private fun setup() {
@@ -22,6 +31,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupToolbar() {
+        binding.toolbar.setupWithNavController(navController)
         setSupportActionBar(binding.toolbar)
     }
 }
