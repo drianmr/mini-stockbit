d16625146fd778bc63f42bfca10f2c0981210675
Adrian M. R <adrianmridwan@gmail.com>
2021-06-06 02:54:05 +0700

    Update top level destination

--- app/src/main/java/com/drianmr/stockbit/mini/ui/main/MainActivity.kt
+++ app/src/main/java/com/drianmr/stockbit/mini/ui/main/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.drianmr.stockbit.mini.R
 import com.drianmr.stockbit.mini.databinding.ActivityMainBinding
@@ -15,6 +17,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var navGraph: NavGraph
+
+    private val topLevelDestination = intArrayOf(
+        R.id.menu_watchlist,
+        R.id.menu_stream,
+        R.id.menu_search,
+        R.id.menu_chat,
+        R.id.menu_portfolio,
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +39,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun init() {
         navController = findNavController(R.id.nav_host_fragment)
+        navGraph = navController.navInflater.inflate(R.navigation.nav_graph_main)
     }
 
     private fun setup() {
@@ -37,22 +49,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupNavController() {
-        val bottomNavigationDestinationIds = intArrayOf(
-            R.id.menu_watchlist,
-            R.id.menu_stream,
-            R.id.menu_search,
-            R.id.menu_chat,
-            R.id.menu_portfolio,
-        )
-
         navController.addOnDestinationChangedListener { _, destination, _ ->
             binding.bottomNavigationView.isGone =
-                (destination.id !in bottomNavigationDestinationIds)
+                (destination.id !in topLevelDestination)
+
+            // Update start destination
+            if (destination.id in topLevelDestination) {
+                if (navGraph.startDestination != R.id.menu_watchlist) {
+                    navGraph.startDestination = R.id.menu_watchlist
+                    navController.graph = navGraph
+                }
+            }
         }
     }
 
     private fun setupToolbar() {
-        binding.toolbar.setupWithNavController(navController)
+        val appBarConfiguration =
+            AppBarConfiguration.Builder(R.id.fragment_login, *topLevelDestination)
+                .build()
+
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
         setSupportActionBar(binding.toolbar)
     }
 
