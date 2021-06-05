aaa4499dce64a2804d1d7c771e30f35e361843c4
Adrian M. R <adrianmridwan@gmail.com>
2021-06-06 01:58:15 +0700

    Add getColorCompat extension function for ViewHolder

--- app/src/main/java/com/drianmr/stockbit/mini/ext/ResourcesExt.kt
+++ app/src/main/java/com/drianmr/stockbit/mini/ext/ResourcesExt.kt
@@ -3,6 +3,10 @@ package com.drianmr.stockbit.mini.ext
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 
 fun Fragment.getColorCompat(@ColorRes color: Int): Int =
     ContextCompat.getColor(requireContext(), color)
+
+fun RecyclerView.ViewHolder.getColorCompat(@ColorRes color: Int): Int =
+    ContextCompat.getColor(itemView.context, color)
